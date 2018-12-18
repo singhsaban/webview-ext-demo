e756b2d555065f9a8a19f602a51f8c8b60e07b68
ssingh <ssingh@carlton.ca>
2018-12-18 11:15:24 -0500

    added all files

---
+++ app/components/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!"/>
+        <GridLayout columns="*" rows="*">
+            <WebViewExt row="0" src="https://www.bbc.com" debugMode="true"></WebViewExt>
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+  import { WebViewExt } from '@nota/nativescript-webview-ext'
+
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
